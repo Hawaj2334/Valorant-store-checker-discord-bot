fec19ad57b9956190ea17747877562168fc3cc1c
Hawaj2334 <97811920+Hawaj2334@users.noreply.github.com>
2022-01-16 03:08:02 +0300

    Update valorant.py

--- cogs/valorant.py
+++ cogs/valorant.py
@@ -15,7 +15,6 @@ from utils.api import ValorantAPI
 
 # #slash_server_id
 MY_SERVER_ID = int(os.getenv('SERVER_ID'))
-MY_REGION = os.getenv('REGION')
 
 class valorant(commands.Cog):
     def __init__(self, bot):
@@ -27,7 +26,7 @@ class valorant(commands.Cog):
     
     @slash_command(description="Shows my daily store", guild_ids=[MY_SERVER_ID]) #ถ้าอยากให้ใช้ได้ทุกเซิฟ เอาออก guild_ids ได้ นะครับ ใช้เวลาประมาณ 1 ชม ถ้าไม่ระบุ หรือ ระบุเซิฟที่บอทอยู่ ประมาณนี้ guild_ids=[929451246493003816, 840379510704046151]
     async def store(self, interaction, username: Option(str, "Input username"), password: Option(str, "Input password")):
-        api = ValorantAPI(interaction, username, password, region=MY_REGION)
+        api = ValorantAPI(interaction, username, password, region='eu')
         await api.start()
         
 def setup(bot):
